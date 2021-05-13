1b6c8a9d0492f2d7ac207101f9f713366f503b57
steve <steve-li-1998@outlook.com>
2021-05-13 07:41:45 +0200

    finish

--- Prog/bs1/bs1/shell_menue.c
+++ Prog/bs1/bs1/shell_menue.c
@@ -1,14 +1,53 @@
-#include<stdbool.h>
+﻿#include<stdbool.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<errno.h>
 
-int main() {
+void runCmd(char *cmd) {
+	pid_t retval = fork();
+	if (retval < 0) {
+		// Fehler
+		perror("Der Prozess kann nich erzeugt werden");
+		exit(errno);
+	}
+	else if (0 == retval) {
+		//Kind
+		if (execlp(cmd, cmd, NULL) < 0) {
+			perror("Das Kommando \"%s\" kann leider nicht durchgeführt werden", cmd);
+			exit(errno);
+		}
+	}
+	else {
+		// Eltern
+		printf("PID von %s: %d\n", cmd, retval);
+	}
+}
+
+
+int main(int argc, char **argv) {
 	bool continueRun = true;
 	while (continueRun)
 	{
 		int option = 0;
 		printf("1. pwd\n2. clear\n3.ls\n4.exit\nAuswahl: ");
-		scanf("%d", &option);
-		printf("%d", option);
+		if (scanf("%s", &option)<1){
+			printf("Fehler bei scanf!\n");
+			exit(1);
+		}
+		char *cmdList[] = { "pwd","clear","ls","exit" };
+		if (option < 1 || option > 4) {
+			printf("Es wurde kein Programm durchgeführt, da Sie eine ungültiges Kommando eingeben!\n");
+			exit(1);
+		}
+		printf("Es wurde %s gewählt\n", cmdList[option - 1]);
+		if (4 == option) {
+			continueRun = false;
+		}
+		else {
+			runCmd(cmdList[option - 1]);
+		}
 	}
 
 	return 0;
